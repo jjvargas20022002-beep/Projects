7d9d4f38577a2c1e595fa6b6359a4cd37b66dfe2
Juan Vargas <jjvargas20022002@gmail.com>
2026-02-16 11:28:35 -0500

    USERS1

--- app.py
+++ app.py
@@ -9,27 +9,32 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "secret_key_temporal")
 
 # =====================
+# GOOGLE SHEET GLOBAL
+# =====================
+sheet = get_sheet()
+
+
+# =====================
 # GOOGLE SHEETS CONFIG
 # =====================
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
 
-creds = Credentials.from_service_account_info(
-    {
-        "type": "service_account",
-        "project_id": os.environ["GCP_PROJECT_ID"],
-        "private_key_id": os.environ["GCP_PRIVATE_KEY_ID"],
-        "private_key": os.environ["GCP_PRIVATE_KEY"].replace("\\n", "\n"),
-        "client_email": os.environ["GCP_CLIENT_EMAIL"],
-        "client_id": os.environ["GCP_CLIENT_ID"],
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.environ["GCP_CLIENT_CERT_URL"],
-    },
-    scopes=SCOPES,
-)
-
-gc = gspread.authorize(creds)
+creds = Credentials.from_service_account_info( 
+   { 
+        "type": "service_account", 
+	"project_id": os.environ["GCP_PROJECT_ID"], 
+	"private_key_id": os.environ["GCP_PRIVATE_KEY_ID"], 
+	"private_key": os.environ["GCP_PRIVATE_KEY"].replace("\\n", "\n"), 
+	"client_email": os.environ["GCP_CLIENT_EMAIL"], 
+	"client_id": os.environ["GCP_CLIENT_ID"], 
+	"auth_uri": "https://accounts.google.com/o/oauth2/auth", 
+	"token_uri": "https://oauth2.googleapis.com/token", 
+	"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs", 
+	"client_x509_cert_url": os.environ["GCP_CLIENT_CERT_URL"], 
+    }, 
+	scopes=SCOPES, 
+) 
+gc = gspread.authorize(creds) 
 sheet = gc.open_by_key(os.environ["SPREADSHEET_ID"])
 
 # =====================
